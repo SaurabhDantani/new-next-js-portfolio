750c3b1d280c54a863ccd54d07493db59d77a025
Saurabh Dantani <62558826+SaurabhDantani@users.noreply.github.com>
2024-03-30 11:53:02 +0000

    update ui

--- src/components/type-effect/IndexTypeEffetc.tsx
+++ src/components/type-effect/IndexTypeEffetc.tsx
@@ -7,30 +7,34 @@ export function IndexTypeEffect() {
   const words = [
     {
       text: "Welcome",
-      className: "text-red-800 dark:text-black break-normal",
+      className: "text-red-800 dark:text-black break-normal md:text-4xl text-xl text-white relative z-20",
     },
     {
-      text: "to",
+      text: "to ",
+      className: "text-yellow-500 dark:text-yello-500 break-normal md:text-4xl text-xl text-white relative z-20",
     },
     {
       text: "my ",
+      className: "text-yellow-500 dark:text-yello-500 break-normal md:text-4xl text-xl text-white relative z-20",
     },
     {
-      text: "portfolio",
+      text: "portfolio ",
+      className: "text-yellow-500 dark:text-yello-500 break-normal md:text-4xl text-xl text-white relative z-20",
     },
     {
       text: "website.",
-      className: "text-blue-500 dark:text-blue-500",
+      className: "text-blue-500 dark:text-blue-500 md:text-4xl text-xl text-white relative z-20",
     },
   ];
 
   const words2 = [
     {
       text: "Hello !",
-      className: "text-yellow-500 dark:text-yello-500 break-normal",
+      className: "text-yellow-500 dark:text-yello-500 break-normal md:text-4xl text-xl text-white relative z-20",
     },
     {
       text: "I'am Saurabh Dantani",
+      className: "text-yellow-500 dark:text-yello-500 break-normal md:text-4xl text-xl text-white relative z-20",
     },  
   ];
   const [delay,setDelay] = useState(false)
