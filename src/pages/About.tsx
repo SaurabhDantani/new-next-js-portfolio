5df24a209de461c9cafd74f26b6d8b0293ff7ab9
Saurabh Dantani <62558826+SaurabhDantani@users.noreply.github.com>
2024-04-14 10:26:55 +0000

    imrove code

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Demox from "@/components/Demox";
 import { StickyScrollRevealDemo } from "@/components/about/AboutIndex";
-import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
 import { IndexBackgroundGradient } from '@/components/mobile-about/IndexLampSection';
 
 export default function About() {
- const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
- useEffect(() => {
+  useEffect(() => {
     // Function to check if the device is a mobile device
     function checkMobileDevice() {
       const mobileViewportWidth = 768; // You can adjust this value based on your needs
@@ -28,24 +28,32 @@ export default function About() {
         setIsMobile(checkMobileDevice());
       });
     };
- }, []);
+  }, []);
 
- return (
-   <div id="about">
-     {!isMobile && (
-       <Card>
-         <CardBody className="items-center">
-           <div className="">
-             <StickyScrollRevealDemo />
-           </div>
-         </CardBody>
-       </Card>
-     )}
-     {isMobile && (
-       <div className="md:hidden lg:hidden p-2">
-         <IndexBackgroundGradient />
-       </div>
-     )}
-   </div>
- );
+  return (
+    <div id="about">
+      {!isMobile && (
+        <Card>
+          <CardBody className="items-center">
+            <div className="">
+              <StickyScrollRevealDemo />
+            </div>
+          </CardBody>
+        </Card>
+      )}
+      {isMobile && (
+        <div className="md:hidden lg:hidden">
+          <Card className="p-0 m-0">
+           <CardHeader className="flex gap-3 text-yellow-400 justify-center text-2xl">
+            About Me
+           </CardHeader>
+            <CardBody className="items-center">
+              <IndexBackgroundGradient />
+            </CardBody>
+          </Card>
+
+        </div>
+      )}
+    </div>
+  );
 }
