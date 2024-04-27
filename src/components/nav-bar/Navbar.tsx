4830fed9ca14fd74d5ef1328aee515772f269e59
Saurabh Dantani <62558826+SaurabhDantani@users.noreply.github.com>
2024-04-27 05:38:17 +0000

    fixed navbar

--- src/components/nav-bar/Navbar.tsx
+++ src/components/nav-bar/Navbar.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem,  Button} from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem,  Button } from "@nextui-org/react";
 import ThemeSwitch from "@/components/theme/ThemeSwitch";
 import Link from "next/link";
-import {useEffect,useState} from 'react'
-// import {AcmeLogo} from "./AcmeLogo.jsx";
+import { useEffect, useState } from 'react'
 
 export default function App() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -28,8 +27,8 @@ export default function App() {
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        {menuItems.map((item) => (
-          <NavbarItem isActive>
+        {menuItems.map((item, index) => ( // Added index parameter
+          <NavbarItem key={index}> {/* Added key prop */}
             <Link href={`#${item}`} aria-current="page">
               {item}
             </Link>
@@ -48,19 +47,18 @@ export default function App() {
       {/* for mobile view */}
       <NavbarMenu>
         {menuItems.map((item, index) => (
-          <div>            
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              color={
-                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              className="w-full"
-              href={`#${item}`}
-
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
+          <div key={index}> {/* Added key prop */}
+            <NavbarMenuItem>
+              <Link
+                color={
+                  index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+                }
+                className="w-full"
+                href={`#${item}`}
+              >
+                {item}
+              </Link>
+            </NavbarMenuItem>
           </div>
         ))}
       </NavbarMenu>
