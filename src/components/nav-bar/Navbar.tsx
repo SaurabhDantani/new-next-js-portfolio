7f239931cb08d8ef2d721e3164c124031632d7b1
Saurabh Dantani <62558826+SaurabhDantani@users.noreply.github.com>
2024-04-27 07:49:28 +0000

    coreext

--- src/components/nav-bar/Navbar.tsx
+++ src/components/nav-bar/Navbar.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem,  Button } from "@nextui-org/react";
+import React, { useState } from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Button } from "@nextui-org/react";
 import ThemeSwitch from "@/components/theme/ThemeSwitch";
 import Link from "next/link";
-import { useEffect, useState } from 'react'
 
 export default function App() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuItems = [
     "intro",
     "about",
     "skills"
   ];
-  const [targetComponentId, setTargetComponentId] = useState(null);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen} shouldHideOnScroll>
@@ -19,36 +21,33 @@ export default function App() {
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="sm:hidden"
+          onClick={toggleMenu} // Toggle menu when clicking the toggle button
         />
         <NavbarBrand>
-          {/* <AcmeLogo /> */}
           <p className="font-bold text-inherit">ACME</p>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        {menuItems.map((item, index) => ( // Added index parameter
-          <NavbarItem key={index}> {/* Added key prop */}
+      <NavbarContent className={`hidden sm:flex gap-4 ${isMenuOpen ? "hidden" : ""}`} justify="center"> {/* Hide NavbarContent when isMenuOpen is true */}
+        {menuItems.map((item, index) => (
+          <NavbarItem key={index}>
             <Link href={`#${item}`} aria-current="page">
               {item}
             </Link>
           </NavbarItem>
         ))}
-
       </NavbarContent>
 
-      {/* theme switch */}
       <NavbarContent justify="end">
         <NavbarItem>
             <ThemeSwitch />
         </NavbarItem>
       </NavbarContent>
 
-      {/* for mobile view */}
       <NavbarMenu>
         {menuItems.map((item, index) => (
-          <div key={index}> {/* Added key prop */}
-            <NavbarMenuItem>
+          <div key={index}>
+            <NavbarMenuItem onClick={toggleMenu}> {/* Toggle menu when clicking menu items */}
               <Link
                 color={
                   index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
